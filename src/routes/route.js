d2316d198a8cf39afb6496ae8a01a56620b9a7e7
shreya <10shreyad@gmail.com>
2022-08-08 16:04:29 +0530

    dependancy installatio

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const _ = require('underscore')
+const lodash  = require('lodash')
 const abc = require('../introduction/intro')
 const greet = require('../logger/logger')
 const x = require('../util/helper')
@@ -19,6 +21,22 @@ router.get('/test-me', function (req, res) {
     format.result
     format2.result2
     format3.result3
+    const arr = ["jan", "feb", "mar", "april", "may", "june", "july", "aug", "sept", "oct", "nov", "dec"]
+    let ans = _.chunk(arr , 3)
+    console.log(ans)
+    const num = [1,3,5,7,9,11,13,15,17,19]
+    let ans2 = lodash.tail(num)
+    console.log(ans2)
+    const dup1 =[1,2,3]
+    const dup2 = [2,3,5]
+    const dup3= [4,5,7]
+    const dup4 = [6,7,8]
+    const dup5 = [8, 4 ,5]
+    let  dupS = _.union(dup1, dup2,dup3, dup4, dup5);
+    console.log(dupS)
+    let pairs =  [['horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']]
+    let obj = lodash.fromPairs(pairs)
+    console.log(pairs)
     res.send('My second ever api!')
 });
 
